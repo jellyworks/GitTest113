47b24b96079e619d862be5ab9c7722dabdd96393
jellyworks1 <jellyworks1@gmail.com>
2023-09-22 14:18:04 +0900

    Initial Commit

---
+++ gitTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitTest1
+//
+//  Created by wizard on 2023/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
